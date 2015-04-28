5b43043da410fa16cc075f705470a9b10b48203a
fernando <fernando.scorpiniti@gmail.com>
2015-04-28 19:54:14 -0300

    Parser de archivos GPS

---
+++ src/ar/edu/untref/industrial/model/Satelite.java
@@ -0,0 +1,28 @@
+package ar.edu.untref.industrial.model;
+
+public class Satelite {
+
+	private int prn;
+	private int elev;
+	private int az;
+	
+	public int getPrn() {
+		return prn;
+	}
+	public void setPrn(int prn) {
+		this.prn = prn;
+	}
+	public int getElev() {
+		return elev;
+	}
+	public void setElev(int elev) {
+		this.elev = elev;
+	}
+	public int getAz() {
+		return az;
+	}
+	public void setAz(int az) {
+		this.az = az;
+	}
+	
+}
